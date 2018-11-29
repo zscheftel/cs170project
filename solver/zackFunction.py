72f365c7863aa4fd31449d510f9818aacfbfa5f7
Zack Scheftel <zscheftel@berkeley.edu>
2018-11-28 20:35:06 -0800

    zackFunction does SOMETHING

--- solver/zackFunction.py
+++ solver/zackFunction.py
@@ -79,13 +79,48 @@ rG = [['1', '5'],
 ]
 filename = 'small'
 
-def solver(G, k, s, rG, filename):
+# def createSupernode(v1, v2):
+#     superName = str(v1) + "," + str(v2)
+#     G.add_node(superName)
+#     friendsToAdd = set([])
+#     for friend in G.adj[v1]:
+#         friendsToAdd.add((superName, friend))
+#     for friend in G.adj[v2]:
+#         friendsToAdd.add((superName, friend))
+#     G.add_edges_from(friendsToAdd)
+#     G.remove_node(v1)
+#     G.remove_node(v2)
+#     return superName
+#
+# def splitSupernode(v):
+#     nodeStrings = v.split(",")
+#     for node in nodeStrings:
+#         G.add_node(node)
+#     G.remove_node(v)
+#     for node in nodeStrings:
+#         for neighbor in graph.adj[node]:
+#             G.add_edge(node, findNeighbor(neighbor))
+#
+# def findNeighbor(v):
+#     for node in list(G.nodes):
+#         if v in node.split(','):
+#             return node
+#
+# G = nx.Graph()
+# G.add_nodes_from(['1', '2', '3', '4'])
+# G.add_edges_from([('1','2'), ('2','3'), ('1', '4')])
+# graph = G.copy()
+# createSupernode('1', '2')
+# createSupernode('1,2', '3')
+# splitSupernode("1,2,3")
+# print(G.nodes, G.edges)
+
+def solver(graph, k, s, rG, filename):
 
     def randomized():
         buses = [[] for i in range(k)]
         rG_sets = [set(x) for x in rG]
-        base = len(list(G.nodes)) // 10
-        counter = base
+        G = graph.copy()
 
         def createSupernode(v1, v2):
             superName = str(v1) + "," + str(v2)
@@ -100,53 +135,77 @@ def solver(G, k, s, rG, filename):
             G.remove_node(v2)
             return superName
 
-        while counter > 0 and len(list(G.edges)) != 0: #try to create supernodes by combining edge members
+        def splitSupernode(v):
+            nodeStrings = v.split(",")
+            for node in nodeStrings:
+                G.add_node(node)
+            G.remove_node(v)
+            for node in nodeStrings:
+                for neighbor in graph.adj[node]:
+                    G.add_edge(node, findNeighbor(neighbor))
 
-            edgeList = list(G.edges)
-            randEdge = edgeList[random.randint(0, len(edgeList)-1)] #Find random edge
+        def findNeighbor(v):
+            for node in list(G.nodes):
+                if v in node.split(','):
+                    return node
 
-            v1 = randEdge[0]
-            sizeV1 = len(v1.split(','))
-            v2 = randEdge[1]
-            sizeV2 = len(v2.split(','))
-
-            #try to add nodes to each bus
-            if (sizeV1 + sizeV2 > s): #overfull bus
-                counter -= 1
-                continue
-
-            subset = False
-
-            for rG_set in rG_sets: #check if a rowdy group is created
-                vTot = v1 + v2
-                if all(x in (vTot).split(',') for x in rG_set):
-                    subset = True
-                    break
-            if subset:
-                counter -= 1
-                continue
-
-            counter = base
-            createSupernode(v1, v2)
-
-        if len(list(G.nodes)) != k: #if we have leftover nodes, we must place them
-
-            lonelyBoiz = [x for x in list(G.nodes) if len(x.split(',')) == 1]
-            superNodes = [x for x in list(G.nodes) if len(x.split(',')) > 1]
-
-
-            currStudentCount = [[i, len(superNodes[i].split(","))] for i in range(len(superNodes))]
+        def placeLeftover():
+            if (len(list(G.nodes)) <= k):
+                return
 
             def sizeSecond(lst):
                 return lst[1]
-            currStudentCount.sort(key=sizeSecond)
 
-            currNode = 0
-            for boi in lonelyBoiz:
-                if len(",".split(superNodes[currNode])) > s:
-                    currNode += 1
-                else:
-                    createSupernode(boi, superNodes[currNode])
+            while len(list(G.nodes)) > k:
+
+                lonelyBoiz = [x for x in list(G.nodes) if len(x.split(',')) == 1]
+                superNodes = [x for x in list(G.nodes) if len(x.split(',')) > 1]
+                currStudentCount = [[i, len(superNodes[i].split(","))] for i in range(len(superNodes))]
+                currStudentCount.sort(key=sizeSecond)
+
+                if len(lonelyBoiz) == 0:
+                    splitSupernode(superNodes[currStudentCount[0][0]])
+
+                lonelyBoiz = [x for x in list(G.nodes) if len(x.split(',')) == 1]
+                superNodes = [x for x in list(G.nodes) if len(x.split(',')) > 1]
+                currStudentCount = [[i, len(superNodes[i].split(","))] for i in range(len(superNodes))]
+                currStudentCount.sort(key=sizeSecond)
+
+                for boi in lonelyBoiz:
+                    superNodes = [x for x in list(G.nodes) if len(x.split(',')) > 1]
+                    currStudentCount = [[i, len(superNodes[i].split(","))] for i in range(len(superNodes))]
+                    currStudentCount.sort(key=sizeSecond)
+                    createSupernode(boi, superNodes[currStudentCount[0][0]])
+
+        while len(list(G.nodes)) > k: #try to create supernodes by combining edge members
+
+            if len(list(G.edges)) != 0:
+                edgeList = list(G.edges)
+                randEdge = edgeList[random.randint(0, len(edgeList)-1)] #Find random edge
+
+                v1 = randEdge[0]
+                sizeV1 = len(v1.split(','))
+                v2 = randEdge[1]
+                sizeV2 = len(v2.split(','))
+
+                #try to add nodes to each bus
+                if (sizeV1 + sizeV2 > s): #overfull bus
+                    continue
+
+                subset = False
+
+                for rG_set in rG_sets: #check if a rowdy group is created
+                    vTot = v1 + v2
+                    if all(x in (vTot).split(',') for x in rG_set):
+                        subset = True
+                        break
+                if subset:
+                    continue
+
+                createSupernode(v1, v2)
+
+            else: #if we have leftover nodes after combining edges, we must place them
+                placeLeftover()
 
         buses = [x.split(',') for x in list(G.nodes)]
         return buses
